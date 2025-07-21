c5f0c4a0d912b9a030621789e1c403851470a76d
damonws <4010136+damonws@users.noreply.github.com>
2025-07-21 16:23:49 -0600

    Sort multiple matches at an address by length of signature.

--- FindCrypt/src/main/java/findcrypt/CryptSignature.java
+++ FindCrypt/src/main/java/findcrypt/CryptSignature.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  *
  * @author torgo
  */
-public class CryptSignature {
+public class CryptSignature implements Comparable<CryptSignature> {
 	private final String name;
 	private final String comment;
 	private final String hexBytes;
@@ -54,6 +54,10 @@ public class CryptSignature {
 		return hexBytes;
 	}
 
+	public int getLength() {
+		return data.length;
+	}
+
 	private static byte[] hexStringToByteArray(String s) {
 		int len = s.length();
 		byte[] data = new byte[len / 2];
@@ -61,6 +65,11 @@ public class CryptSignature {
 			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
 		}
 		return data;
+	}
+
+	@Override
+	public int compareTo(CryptSignature o) {
+		return o.getLength() - getLength();
 	}
 
 }
